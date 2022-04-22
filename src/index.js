2521867040d4d0b6ca8ca809e91f51c5ad3aae8b
TylerLott <tylerlott0126@gmail.com>
2022-04-21 22:54:25 -0600

    work

--- src/index.js
+++ src/index.js
@@ -27,25 +27,45 @@ let repl = mongoose.createConnection(MONGO_URL_REPL, {
 // should edit the viewer events
 const VIEWER_EVENTS = primary.model("ViewerEvents", {
   team: String,
-  timestamp: String,
-})
-// should track the teams, players, and Viewer settings
-const TEAM_REPL = repl.model("Team", {
-  name: String,
-  players: String,
-})
-const PLAYER_REPL = repl.model("Player", {
-  name: String,
-})
-const VIEWER_SETTINGS = repl.model("ViewerSettings", {
-  viewerTimeout: String,
-  threshold: String,
-  teamCooldown: String,
+  active: Boolean,
 })
 
-// APP LOGIC
-// REDIS
-;async () => {
+// should track the teams, players, and Viewer settings
+const TEAM_REPL = repl.model(
+  "team",
+  {
+    name: String,
+    players: [String],
+    createdBy: String,
+  },
+  "teams"
+)
+
+const PLAYER_REPL = repl.model(
+  "user",
+  {
+    _id: String,
+    type: String,
+    streamerName: String,
+    gameName: String,
+    attributes: {
+      kills: Number,
+      placement: Number,
+    },
+    active: Boolean,
+  },
+  "users"
+)
+const SETTINGS_REPL = repl.model(
+  "setting",
+  {
+    viewerTimeout: String,
+    threshold: String,
+    teamCooldown: String,
+  },
+  "settings"
+)
+;(async () => {
   // Create clients
   const redisPub = redis.createClient({
     url: REDIS_PATH,
@@ -71,26 +91,73 @@ const VIEWER_SETTINGS = repl.model("ViewerSettings", {
   await redisSub.connect()
   await redisSub.subscribe("team-timeouts", (message, chan) => {
     // publish viewer-event in mongo
+    let mes = JSON.parse(message)
+    if (mes.val > 0) {
+      let event = new VIEWER_EVENTS({
+        team: mes.team,
+        active: true,
+      })
+      event.save()
+      let sets = SETTINGS_REPL.findOne()
+      setTimeout(async () => {
+        VIEWER_EVENTS.updateOne(
+          { team: mes.team },
+          {
+            active: false,
+          }
+        )
+        redisPub.publish(
+          "team-timeouts",
+          JSON.stringify({ team: mes.team, val: 0 })
+        )
+      }, sets.teamCooldown)
+    }
   })
   // Mongo
   // track the teams
-  TEAM_REPL.watch().on("change", (data) => {
-    // set redis teams
-    // publish teams to teams channel
-    console.log("data", data)
+  TEAM_REPL.watch().on("change", async (data) => {
+    console.log("teams", data)
+    if (typeof data === "string" || data instanceof String) {
+      console.log("teams change", data)
+    } else {
+      // set redis settings
+      await redisPub.set("teams", JSON.stringify(data.fullDocument))
+      redisPub.publish("teams", JSON.stringify(data.fullDocument))
+      // publish settings to the settings channel
+      console.log("changing players", data)
+    }
   })
   // track the players
-  PLAYER_REPL.watch().on("change", (data) => {
-    // set redis players
-    // publish players to players channel
-    console.log("data", data)
+  PLAYER_REPL.watch().on("change", async (data) => {
+    console.log("players", data)
+    if (typeof data === "string" || data instanceof String) {
+      console.log("players change", data)
+    } else {
+      // set redis settings
+      let plays = PLAYER_REPL.find({ active: true, type: "player" }).then(
+        (users) => {
+          console.log("setting redis", users)
+          redisPub.set("players", JSON.stringify(users)).then(() => {
+            redisPub.publish("players", JSON.stringify(users))
+          })
+          // publish settings to the settings channel
+        }
+      )
+    }
   })
   // track the settings
-  SETTINGS_REPL.watch().on("change", (data) => {
-    // set redis settings
-    // publish settings to the settings channel
-    console.log("data", data)
+  SETTINGS_REPL.watch().on("change", async (data) => {
+    console.log("settings", data)
+    if (data && data.operationType === "insert") {
+      if (typeof data === "string" || data instanceof String) {
+        console.log("settings change", data)
+      } else {
+        // set redis settings
+        await redisPub.set("settings", JSON.stringify(data.fullDocument))
+        redisPub.publish("settings", JSON.stringify(data.fullDocument))
+      }
+    }
   })
   const app = express()
   app.listen(PORT, () => console.log(`listening on port ${PORT}`))
-}
+})()
