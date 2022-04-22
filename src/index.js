ba0466ab291c44c5152c2a57a4ce1069d1e66ae7
TylerLott <tylerlott0126@gmail.com>
2022-04-21 20:06:13 -0600

    update

--- src/index.js
+++ src/index.js
@@ -3,21 +3,17 @@ import { promisify } from "util"
 import redis from "redis"
 import mongoose from "mongoose"
 
-const PORT = 8080
-const REDIS_HOST = ""
-const REDIS_PORT = 6379
-const MONGO_URL_PRIMARY = ""
-const MONGO_URL_REPL = ""
+let PORT = 80
+let REDIS_PATH = ""
+let MONGO_URL_PRIMARY = ""
+let MONGO_URL_REPL = ""
 
-// REDIS
-const redisPub = redis.createClient({
-  host: REDIS_HOST,
-  port: REDIS_PORT,
-})
-const redisSub = redis.createClient({
-  host: REDIS_HOST,
-  port: REDIS_PORT,
-})
+if (process.env.NODE_ENV !== "production") {
+  PORT = 8080
+  REDIS_PATH = "redis://44.204.86.55:6379"
+  MONGO_URL_PRIMARY = "mongodb://3.82.232.239:27017/monday"
+  MONGO_URL_REPL = "mongodb://3.82.232.239:27018/monday"
+}
 
 // MONGO
 let primary = mongoose.createConnection(MONGO_URL_PRIMARY, {
@@ -48,25 +44,53 @@ const VIEWER_SETTINGS = repl.model("ViewerSettings", {
 })
 
 // APP LOGIC
-
-// Redis
-// track the teams on redis
-redisSub.on("testKey", (key, value) => {
-  console.log("key", key, "value", value)
-})
-// publish to redis when teams change
-redisPub.publish("testkey", "testvalue")
-// publish to redis when players change
-// publish to redis when threshold is hit
-// publish to redis when settings are changed
-
-// Mongo
-// track the teams
-TEAM_REPL.watch().on("change", (data) => console.log("data", data))
-// track the players
-// track the settings
-// publish an event when threshold is hit
-
-const app = express()
-
-app.listen(PORT, () => console.log(`listening on port ${PORT}`))
+// REDIS
+;async () => {
+  // Create clients
+  const redisPub = redis.createClient({
+    url: REDIS_PATH,
+  })
+  const redisSub = redis.createClient({
+    url: REDIS_PATH,
+  })
+  // Error and Ready
+  redisPub.on("error", (err) => {
+    console.log("error connecting to redis", err)
+  })
+  redisSub.on("error", (err) => {
+    console.log("error connecting to redis", err)
+  })
+  redisPub.on("ready", () => {
+    console.log("connected to redis")
+  })
+  redisSub.on("ready", () => {
+    console.log("connected to redis")
+  })
+  // connect
+  await redisPub.connect()
+  await redisSub.connect()
+  await redisSub.subscribe("team-timeouts", (message, chan) => {
+    // publish viewer-event in mongo
+  })
+  // Mongo
+  // track the teams
+  TEAM_REPL.watch().on("change", (data) => {
+    // set redis teams
+    // publish teams to teams channel
+    console.log("data", data)
+  })
+  // track the players
+  PLAYER_REPL.watch().on("change", (data) => {
+    // set redis players
+    // publish players to players channel
+    console.log("data", data)
+  })
+  // track the settings
+  SETTINGS_REPL.watch().on("change", (data) => {
+    // set redis settings
+    // publish settings to the settings channel
+    console.log("data", data)
+  })
+  const app = express()
+  app.listen(PORT, () => console.log(`listening on port ${PORT}`))
+}
